a2dda2700756df4691b5e47bd7fcb8a7983f57d0
Miles Noble <MilosDragons@gmail.com>
2023-09-15 11:38:26 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+Console.CursorVisible = false;
+
+List<ConsoleColor> foregroundColors = new List<ConsoleColor>() {
+    ConsoleColor.DarkBlue, ConsoleColor.DarkGreen, ConsoleColor.DarkCyan,
+    ConsoleColor.DarkRed, ConsoleColor.DarkMagenta, ConsoleColor.DarkYellow,
+    ConsoleColor.Gray, ConsoleColor.DarkGray, ConsoleColor.Blue,
+    ConsoleColor.Green, ConsoleColor.Cyan, ConsoleColor.Red,
+    ConsoleColor.Magenta, ConsoleColor.Yellow
+};
+string helloWorld = "Hello World";
+int currentColorsIndex = 0;
+int leftMargin = (Console.WindowWidth - helloWorld.Length) / 2;
+int topMargin = Console.WindowHeight / 2;
+
+while (!Console.KeyAvailable)
+{
+    Console.SetCursorPosition(leftMargin, topMargin); // set the cursor to the center of the console
+    Console.ForegroundColor = foregroundColors[currentColorsIndex]; // rotate through the foreground colors
+    currentColorsIndex = currentColorsIndex >= foregroundColors.Count - 1 ? 0 : currentColorsIndex + 1; // increment the colors index
+
+    Console.Write(helloWorld); // write Hello World to the console
+
+    Thread.Sleep(250); // wait a 1/4 second before running the code again
+}
+
+Console.ResetColor();
+Console.Clear();
---
+++ README.md
@@ -0,0 +1,23 @@
+# Overview
+
+{Important!  Do not say in this section that this is college assignment.  Talk about what you are trying to accomplish as a software engineer to further your learning.}
+
+My software is a simple hello world program that prints "Hello World" to the Console. My program is unique because it changes colors every quarter second until the user presses a key to end the program.
+
+{Describe your purpose for creating this software.}
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Development Environment
+
+For this project I used Visual Studios to develop my code. I wrote my Code in C# which is a object oriented programing language.
+
+{Describe the tools that you used to develop the software}
+
+{Describe the programming language that you used}
+
+# Useful Websites
+
+I did not use any websites when making this project but some I have used in the past include:
+* [W3Schools](https://www.w3schools.com/)
+* [Stack Overflow](https://stackoverflow.com/)
